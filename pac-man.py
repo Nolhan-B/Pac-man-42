9d0a9f6ea6e52c3f60c4cb9bc8bc63201a76ff60
nbilyj <nbilyj@k2r3p9.42mulhouse.fr>
2026-04-28 12:55:10 +0200

    player class done + adjusted main (pac-man.py)

--- pac-man.py
+++ pac-man.py
@@ -1,17 +1,17 @@
 import logging
 logging.basicConfig(level=logging.WARNING)
-
+import sys
 from parser import ConfigLoader
 
-loader = ConfigLoader("config.json")
-loader.load()
-print(loader.filepath)
-print(loader.highscore_filename)
-print(loader.lives)
-print(loader.pacgum)
-print(loader.points_per_super_pacgum)
-print(loader.points_per_pacgum)
-print(loader.points_per_ghost)
-print(loader.seed)
-print(loader.level_max_time)
-print(loader.levels)
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: python3 pac-man.py <config_file.json>")
+        raise SystemExit(1)
+    
+    config = ConfigLoader(sys.argv[1])
+    config.load()
+
+	#lancer le fichier
+
+if __name__ == "__main__":
+    main()
--- player.py
+++ player.py
@@ -1,13 +1,44 @@
 class Player:
-	def __init__(config: ConfigLoader) -> None:
+	def __init__(self, config: ConfigLoader) -> None:
 		self.lives = config.lives
-		self.__pos_x: int = None
-		self.__pos_y: int = None
+		self._pos_x: int = None
+		self._pos_y: int = None
+		self.score: int = 0
 
 	def _init_player_pos(self, maze_size_x: int, maze_size_y: int) -> None:
-		self.__pos_x = maze_size_x // maze_size_y
+		self._pos_x = maze_size_x // 2
+		self._pos_y = maze_size_y // 2
 	
 	def move_up(self) -> None:
+		self._pos_y += 1
 
+	def move_down(self) -> None:
+		self._pos_y -= 1
 
-	def set_position(self)
+	def move_right(self) -> None:
+		self._pos_x += 1
+
+	def move_left(self) -> None:
+		self._pos_x -= 1
+
+	def set_position(self, x: int, y: int) -> None:
+		self._pos_x = x
+		self._pos_y = y
+	
+	def get_position(self) -> tuple[int, int]:
+		return (self._pos_x, self._pos_y)
+
+	def get_pos_x(self) -> int:
+		return self._pos_x
+
+	def get_pos_y(self) -> int:
+		return self._pos_y
+
+	def lose_life(self) -> None:
+		if self.lives == 0:
+			logger.warning("Player can't lose life, already at 0 !")
+		else:
+			self.lives -= 1
+
+	def add_score(self, amount: int) -> None:
+		self.score += amount
