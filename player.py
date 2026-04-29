cfd6c0fed76799957b2be85926f604a5dc6a05af
nbarbosa <nbarbosa@student.42mulhouse.fr>
2026-04-29 14:38:41 +0200

    jai refait la classe player qui etait a chier

--- player.py
+++ player.py
@@ -1,7 +1,15 @@
+from ghost import Direction
 from parser import ConfigLoader
 import logging
 logger = logging.getLogger(__name__)
 
+
+OPPOSITES = {
+    Direction.NORTH: Direction.SOUTH,
+    Direction.SOUTH: Direction.NORTH,
+    Direction.EAST: Direction.WEST,
+    Direction.WEST: Direction.EAST
+    }
 
 class Player:
     def __init__(self, config: ConfigLoader) -> None:
@@ -9,21 +17,28 @@ class Player:
         self._pos_x: int = None
         self._pos_y: int = None
         self.score: int = 0
+        self.current_direction: Direction = None
+        self.next_direction: Direction = None
+        self.move_timer: float = 0.0
 
     def _init_player_pos(self, maze_size_x: int, maze_size_y: int) -> None:
         self._pos_x = maze_size_x // 2
         self._pos_y = maze_size_y // 2
 
     def move_up(self) -> None:
-        self._pos_y += 1
-
-    def move_down(self) -> None:
+        self.current_direction = Direction.NORTH
         self._pos_y -= 1
 
+    def move_down(self) -> None:
+        self.current_direction = Direction.SOUTH
+        self._pos_y += 1
+
     def move_right(self) -> None:
+        self.current_direction = Direction.EAST
         self._pos_x += 1
 
     def move_left(self) -> None:
+        self.current_direction = Direction.WEST
         self._pos_x -= 1
 
     def set_position(self, x: int, y: int) -> None:
@@ -45,5 +60,58 @@ class Player:
         else:
             self.lives -= 1
 
+    def set_next_direction(self, direction: Direction) -> None:
+        self.next_direction = direction
+
     def add_score(self, amount: int) -> None:
         self.score += amount
+
+    def _execute_move(self) -> None:
+        if self.current_direction == Direction.NORTH:
+            self.move_up()
+        elif self.current_direction == Direction.SOUTH:
+            self.move_down()
+        elif self.current_direction == Direction.WEST:
+            self.move_left()
+        elif self.current_direction == Direction.EAST:
+            self.move_right()
+
+    def _can_move(self, direction, layout) -> bool:
+
+        pos_x, pos_y = self._pos_x, self._pos_y
+        val = layout[pos_y][pos_x]
+
+        # Check Nord (Bit 1)
+        if direction == Direction.NORTH and (val & 1) == 0:
+            return True
+
+        # Check Est (Bit 2)
+        elif direction == Direction.EAST and (val & 2) == 0:
+            return True
+
+        # Check Sud (Bit 4)
+        elif direction == Direction.SOUTH and (val & 4) == 0:
+            return True
+
+        # Check Ouest (Bit 8)
+        elif direction == Direction.WEST and (val & 8) == 0:
+            return True
+
+        return False
+
+    def update_player(self, layout: list[list[int]]) -> None:
+        self.move_timer += 1.0
+        time_to_move = 30.0
+        if self.move_timer >= time_to_move:
+            #  On essaye de tourner vers la direction demandée (next_dir)
+            if self._can_move(self.next_direction, layout):
+                self.current_direction = self.next_direction
+            #  On vérifie si on peut avancer dans la direction actuelle
+            if self._can_move(self.current_direction, layout):
+                self._execute_move(self.current_direction)
+                self.move_timer = 0.0
+            else:
+                # On bute contre un mur, on attend une nouvelle direction
+                self.move_timer = 0.0
+
+        
