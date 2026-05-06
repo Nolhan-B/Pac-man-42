437326445fe0711bce98bf45cb15b1ae346dc6f1
nbarbosa <nbarbosa@student.42mulhouse.fr>
2026-05-06 12:51:52 +0200

    refactor et norme

--- bug.md
+++ bug.md
@@ -1,12 +1,4 @@
 # Fantômes
 
-les fantomes repasse en frightened quqnd pac man prend une super gum alors quils sont dead et pas encore respawn
 
 Note :
-jai pas encore essayer de deconstruire mon idee mais,
-utiliser genre un module pour avoir le timestamp et genre
-en fonction de leur vitesse, enregistrer le time a laquelle il 
-arrive sur sa case a chaque fois, joueur comme fantome,
-comme ca si on etablit que visuellement genre il faudrait 0.5s par exemple
-pour que le fantome touche le joueurs ou linverse, on calcul a chaque case pour voir
-si il a bien touché le fantome ou inverse, pour voir sil faut faire quelque chose ou pas 
--- player.py
+++ player.py
@@ -106,9 +106,12 @@ class Player:
         return False
 
     def update_player(self, layout: list[list[int]]) -> None:
-        if self.current_direction and self.next_direction == OPPOSITES.get(self.current_direction):
-
-            if self.move_timer > 0 and self._can_move(self.current_direction, layout):
+        if self.current_direction and (
+            self.next_direction == OPPOSITES.get(self.current_direction)
+        ):
+            if self.move_timer > 0 and self._can_move(
+                self.current_direction, layout
+            ):
                 self._execute_move()
                 self.move_timer = self.speed - self.move_timer
             else:
@@ -119,62 +122,30 @@ class Player:
             return
 
         if self.move_timer == 0.0:
-            if self.next_direction and self._can_move(self.next_direction, layout):
+            if self.next_direction and self._can_move(
+                self.next_direction, layout
+            ):
                 self.current_direction = self.next_direction
                 self.next_direction = None
 
             # Si on fait face à un mur et qu'on ne peut pas avancer
-            if self.current_direction and not self._can_move(self.current_direction, layout):
+            if self.current_direction and not self._can_move(
+                self.current_direction, layout
+            ):
                 return
 
         self.move_timer += 1.0
 
         if self.move_timer >= self.speed:
-            if self.current_direction and self._can_move(self.current_direction, layout):
+            if self.current_direction and self._can_move(
+                self.current_direction, layout
+            ):
                 self._execute_move()
 
-            if self.next_direction and self._can_move(self.next_direction, layout):
+            if self.next_direction and self._can_move(
+                self.next_direction, layout
+            ):
                 self.current_direction = self.next_direction
                 self.next_direction = None
 
             self.move_timer = 0.0
-    # def update_player(self, layout: list[list[int]]) -> None:
-    #     # 1. Le Demi-Tour sécurisé (Zéro passe-muraille)
-    #     if self.current_direction and self.next_direction == OPPOSITES.get(self.current_direction):
-
-    #         if self.move_timer > 0 and self._can_move(self.current_direction, layout):
-    #             self._execute_move()
-    #             self.move_timer = self.speed - self.move_timer
-    #         else:
-    #             # Si on était bloqué (timer à 0), on pivote juste sur place
-    #             self.move_timer = 0.0
-
-    #         self.current_direction = self.next_direction
-    #         self.next_direction = None
-    #         return
-
-    #     self.move_timer += 1.0
-
-    #     if self.move_timer >= self.speed:
-    #         if self.current_direction and self._can_move(self.current_direction,
-    #                                                      layout):
-    #             self._execute_move()
-
-    #         if self._can_move(self.next_direction, layout):
-    #             self.current_direction = self.next_direction
-
-            self.move_timer = 0.0
-    # def update_player(self, layout: list[list[int]]) -> None:
-    #     self.move_timer += 1.0
-    #     time_to_move = 30.0
-    #     if self.move_timer >= time_to_move:
-    #         #  On essaye de tourner vers la direction demandée (next_dir)
-    #         if self._can_move(self.next_direction, layout):
-    #             self.current_direction = self.next_direction
-    #         #  On vérifie si on peut avancer dans la direction actuelle
-    #         if self._can_move(self.current_direction, layout):
-    #             self._execute_move()
-    #             self.move_timer = 0.0
-    #         else:
-    #             # On bute contre un mur, on attend une nouvelle direction
-    #             self.move_timer = 0.0
